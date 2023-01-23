a442c20d839044763b52448feff7f4b51c6fc090
sezim <osmsezimka@gmail.com>
2023-01-24 01:02:53 +0600

    add serializer and view | change url(but i'll try check create some elements)

---
+++ main/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import *
+
+
+class GenreSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+
+class FilmSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Film
+        fields = '__all__'
--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny
+
+from .models import *
+from .serializers import FilmSerializers, GenreSerializers
+
 
 # Create your views here.
+
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializers
+    permission_classes = [AllowAny, ]
+
+
+class FilmViewSet(viewsets.ModelViewSet):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializers
+    permission_classes = [AllowAny, ]
+
